ae6be018e1b7fb899903b565dd3d4b7a7e9348d9
Abir Talukder <166286745+abx667@users.noreply.github.com>
2024-04-17 08:43:11 -0700

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -2,7 +2,7 @@ const mongoose =require('mongoose');
 //const username=process.env.MONGODB_USERNAME;
 //const password=process.env.MONGODB_PASSWORD;
 
- mongoose.connect("mongodb+srv://shreayas:shreayasd123@cluster0.xjxerxi.mongodb.net/registration-form").then(()=>{
+ mongoose.connect("mongodb+srv://tabir327:tabir123@cluster0.3gdyfvw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
     console.log("connection established");
 }).catch((error)=>{
     console.log(error);
