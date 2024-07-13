0e9865f61e67987a48089d5ef82e7dd0875147f5
Abir Talukder <166286745+abx667@users.noreply.github.com>
2024-07-14 00:42:36 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -167,18 +167,19 @@ app.get("/",(req,res)=>{
   res.render('index');
 });
 
-const course=['seo','python','java','jdbc','jquery','laravel','c','c++','mysq','Node','php','plsql'];
-let i=0;
+
+
 
 app.post("/search", (req, res) => {
-
+const course=['seo','python','java','jdbc','jquery','laravel','c','c++','mysq','Node','php','plsql'];
+let i=0;
 let found=false;
   const searchname = req.body.searchname;
   if(req.session.email){
   for(i=0;i<course.length;i++){
   if(searchname.toLowerCase().equals(course[i])){
     found=true;
-    res.render(searchname.toLowercase());
+    res.render(course[i]);
       
       break;
   }
